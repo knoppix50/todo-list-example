72e7b53a7d96781f552ab230051975d329cf9097
EC2 Default User <ec2-user@ip-172-31-62-113.ec2.internal>
2021-06-12 14:43:45 +0000

    Version mejorada con nueva función para traducir el contenido de la tarea.

--- todos/translate.py
+++ todos/translate.py
@@ -1,28 +1,30 @@
 import os
 import json
-import logging
+
 from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
-def translate(event, context):
-    translater = boto3.client('translate')
+def getTranslated(event, context):
+    translate = boto3.client('translate')
+    source_language = 'en'
+    target_language = event['pathParameters']['target_language']
     
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    target_language = event['pathParameters']['id']['target_language']
-    # fetch todo from the database
-    result = table.get_item(
+    recordset = table.get_item(
         Key={
             'id': event['pathParameters']['id']
         }
     )
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(result['Item'], cls=decimalencoder.DecimalEncoder, target_language)
-    }
-
-    return response
+    texto = (recordset['Item']['text'])
     
-
-   
+    #result = translater.translate_text(Text='Hello, my name is Alfredo.', SourceLanguageCode=source_language, TargetLanguageCode=target_language)
+    result = translate.translate_text(Text=texto, SourceLanguageCode=source_language, TargetLanguageCode=target_language)
+    
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(result['TranslatedText'])
+        
+    }
+    
+    return response
